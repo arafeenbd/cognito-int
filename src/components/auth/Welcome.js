c34cfb3f1e434b5435596789eb35f8ba6347abc5
arafeen <arafeenbd@yahoo.com>
2021-03-07 21:33:32 -0500

    second commit

---
+++ src/components/auth/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section className="section auth">
+      <div className="container">
+        <h1>Welcome!</h1>
+        <p>You have successfully registered a new account.</p>
+        <p>We have send you an email. Please click the link to complete sign up</p>
+      </div>
+    </section>
+  )
+}
